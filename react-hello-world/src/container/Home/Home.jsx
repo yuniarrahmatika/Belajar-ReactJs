4ecf56b4bd3556d6d50731f336a2035ff592d0c2
yuniarrahmatika <yuniarrahmatika@gmail.com>
2021-07-22 05:26:38 +0700

    LifeCycle Component

--- react-hello-world/src/container/Home/Home.jsx
+++ react-hello-world/src/container/Home/Home.jsx
@@ -1,8 +1,21 @@
 import React, {Component} from 'react';
-import YouTubeComp from '../../component/YouTubeComp/YouTubeComp';
-import Product from '../Product/Product';
+// import YouTubeComp from '../../component/YouTubeComp/YouTubeComp';
+import LifeCycleComponent from '../LifeCycleComp/LifeCycleComp';
+// import Product from '../Product/Product';
 
 class Home extends Component{
+    state = {
+        showComponent: true
+    }
+
+    componentDidMount(){
+        // setTimeout(()=>{
+        //     this.setState({
+        //         showComponent: false
+        //     })
+        // }, 15000)        
+    }
+
     render(){
         return(
             <div>
@@ -25,9 +38,16 @@ class Home extends Component{
                     title="Tutorial React Js - Bagian 4" 
                     desc="1k ditonton, 14 hari yang lalu"/>
                 <YouTubeComp/> */}
-                <p>Counter</p>
+                {/* <p>Counter</p>
                 <hr/>
-                <Product />
+                <Product /> */}
+                <p>LifeCycle Component</p>
+                <hr/>
+                {
+                    this.state.showComponent ?
+                    <LifeCycleComponent/>
+                    : null
+                }
             </div>            
         )
     }
---
+++ react-hello-world/src/container/LifeCycleComp/LifeCycleComp.jsx
@@ -0,0 +1,66 @@
+import React, {Component} from 'react';
+import './LifeCycleComp.css';
+
+class LifeCycleComponent extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            count: 1
+        }
+        console.log('constructor')
+    }
+
+    static getDerivedStateFromProps(props, state){
+        console.log('getDerivedStateFromProps')
+        return null;
+    }
+
+    componentDidMount(){
+        console.log('componentDidMount')
+        // setTimeout(()=>{
+        //     this.setState({
+        //         count: 2
+        //     })
+        // }, 5000)
+    }
+
+    shouldComponentUpdate(nextProps, nextState){
+        console.group('shouldComponentUpdate')
+        // console.log('nextProps',nextProps);
+        console.log('nextState',nextState);
+        console.log('this state',this.state);
+        console.groupEnd();
+        if(nextState.count >= 4){
+            return false;
+        }
+        return true;
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState){
+        console.log('getSnapshotBeforeUpdate')
+        return null;
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot){
+        console.log('componentDidUpdate')
+    }
+
+    componentWillUnmount(){
+        console.log('componentWillUnmount')
+    }
+
+    changeCount = () => {
+        this.setState({
+            count: this.state.count + 1
+        })
+    }
+
+    render(){
+        console.log('render')
+        return(
+            <button className="btn" onClick={this.changeCount}>Component Button {this.state.count}</button>
+        )
+    }
+}
+
+export default LifeCycleComponent;
